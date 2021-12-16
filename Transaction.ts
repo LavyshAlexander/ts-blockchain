27e9f22680cc000dff52892804ec1d28b78ff208
Lavysh Alexander <LavyshAlexander@gmail.com>
2021-12-16 19:52:06 +0300

    Added Transaction class.

---
+++ Transaction.ts
@@ -0,0 +1,12 @@
+export class Transaction {
+	constructor(
+		public amount: number,
+		public payer: string,
+		public payee: string
+	) {
+	}
+
+	toString() {
+		return JSON.stringify(this)
+	}
+}
